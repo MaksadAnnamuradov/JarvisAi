234f8dbcf1fd581c6ffb5054cc35470a59df0ea6
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-03-05 10:53:27 -0700

    fixed some issues with voice recognition

--- initial_try.py
+++ initial_try.py
@@ -19,13 +19,13 @@ speaker = tts.init()
 speaker.setProperty('rate', 150)
 
 #create to do list of string
-todo_list = ["Go shopping", "Clean Room", "Record Video"]
+todo_list = ["go shopping", "clean room", "record video"]
 
 
-def create_note(text):
+#save notes to file
+def save_notes():
     global recognizer
-
-    speaker.say("What would you like to call your note?")
+    speaker.say("What file would you like to save your notes to?")
     speaker.runAndWait()
 
     done = False
@@ -33,24 +33,18 @@ def create_note(text):
         
         try:
             with sr.Microphone() as source:
-                audio = recognizer.listen(source)
-
-                note_name = recognizer.recognize_google(audio)
-                note_name = note_name.lower()
-
-                speaker.say("Choose a file to save your note to.")
-                speaker.runAndWait()
-                
-                recognizer.adjust_for_ambient_noise(source, duration=0.2)
-                audio = recognizer.listen(source)
+                # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+                audio = recognizer.listen(source, phrase_time_limit=2)
 
                 filename = recognizer.recognize_google(audio)
                 filename = filename.lower()
-            with open(filename, "w") as file:
-                file.write(text)
-                done = True
-                speaker.say("Note created.")
-                speaker.runAndWait()
+
+                with open(filename + ".txt", "w") as file:
+                    for item in todo_list:
+                        file.write(item + "\n")
+                    done = True
+                    speaker.say("Notes saved.")
+                    speaker.runAndWait()
                 
         except sr.UnknownValueError:
             recognizer = sr.Recognizer()
@@ -58,7 +52,6 @@ def create_note(text):
             speaker.runAndWait()
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
 
 def add_todo():
     global recognizer
@@ -71,15 +64,21 @@ def add_todo():
         
         try:
             with sr.Microphone() as source:
-                audio = recognizer.listen(source)
+                # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+                audio = recognizer.listen(source, phrase_time_limit=2)
 
                 note_name = recognizer.recognize_google(audio)
                 note_name = note_name.lower()
 
-                todo_list.append(note_name)
-                done = True
-                speaker.say("Note added.")
-                speaker.runAndWait()
+                #check if note already exists in to do list
+                if note_name in todo_list:
+                    speaker.say("That note already exists.")
+                    speaker.runAndWait()
+                else:
+                    todo_list.append(note_name)
+                    done = True
+                    speaker.say("Note added.")
+                    speaker.runAndWait()
                 
         except sr.UnknownValueError:
             recognizer = sr.Recognizer()
@@ -89,7 +88,7 @@ def add_todo():
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 def show_todos():
-    global speaker
+    global recognizer
     speaker.say("Here is your to do list.")
     speaker.runAndWait()
     for item in todo_list:
@@ -98,6 +97,7 @@ def show_todos():
 
 def remove_todo():
     global recognizer
+
     speaker.say("What would you like to remove from your to do list?")
     speaker.runAndWait()
 
@@ -106,15 +106,22 @@ def remove_todo():
         
         try:
             with sr.Microphone() as source:
-                audio = recognizer.listen(source)
+                # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+                audio = recognizer.listen(source, phrase_time_limit=2)
 
                 note_name = recognizer.recognize_google(audio)
                 note_name = note_name.lower()
 
-                todo_list.remove(note_name)
-                done = True
-                speaker.say("Note removed.")
-                speaker.runAndWait()
+
+                # check if the note does not exist in to do list
+                if note_name not in todo_list:
+                    speaker.say("Sorry, that note does not exist.")
+                    speaker.runAndWait()
+                else:
+                    todo_list.remove(note_name)
+                    done = True
+                    speaker.say("Note removed.")
+                    speaker.runAndWait()
                 
         except sr.UnknownValueError:
             recognizer = sr.Recognizer()
@@ -126,7 +133,8 @@ def remove_todo():
 #create a function to edit to do list
 def edit_todo():
     global recognizer
-    speaker.say("What would you like to edit on your to do list?")
+
+    speaker.say("What is the name of to do that you would like to edit?")
     speaker.runAndWait()
 
     done = False
@@ -134,25 +142,29 @@ def edit_todo():
         
         try:
             with sr.Microphone() as source:
-                audio = recognizer.listen(source)
+                # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+                audio = recognizer.listen(source, phrase_time_limit=2)
 
-                note_name = recognizer.recognize_google(audio)
-                note_name = note_name.lower()
+                todo_edit = recognizer.recognize_google(audio)
+                todo_edit = todo_edit.lower()
 
-                speaker.say("What would you like to change it to?")
-                speaker.runAndWait()
+                # check if the note does not exist in to do list
+                if todo_edit not in todo_list:
+                    speaker.say("Sorry, that note does not exist.")
+                    speaker.runAndWait()
+                else:
+                    speaker.say("What would you like to change it to?")
+                    speaker.runAndWait()
 
-                with sr.Microphone() as source:
-                    audio = recognizer.listen(source)
+                    editted_todo = recognizer.recognize_google(audio)
+                    editted_todo = editted_todo.lower()
 
-                    note_name = recognizer.recognize_google(audio)
-                    note_name = note_name.lower()
 
-                    todo_list[todo_list.index(note_name)] = note_name
+                    todo_list[todo_list.index(todo_edit)] = editted_todo
                     done = True
                     speaker.say("Note edited.")
                     speaker.runAndWait()
-                
+            
         except sr.UnknownValueError:
             recognizer = sr.Recognizer()
             speaker.say("Sorry, I didn't get that.")
@@ -172,40 +184,39 @@ def quit():
     sys.exit()
 
 mappings = {
-    "create note": create_note,
+    "save notes": save_notes,
     "add to do": add_todo,
     "show to do": show_todos,
     "remove to do": remove_todo,
     "edit to do": edit_todo,
-    "quit": quit,
-    "hello": hello
+    "exit": quit,
+    "greeting": hello
 }
 
 assistant = GenericAssistant('intents.json', model_name = "test_mode", intent_methods = mappings)
-assistant.train_model()
+# assistant.train_model()
 # assistant.save_model()
 
-# assistant.load_model("test_mode")
+assistant.load_model("test_mode")
 
 while True:
     try:
         with sr.Microphone() as source:
-            speaker.say("Welcome!")
+            speaker.say("How can I help you?")
             speaker.runAndWait()
             # print("Say something!")
-            recognizer.adjust_for_ambient_noise(source, duration=0.2)
-            audio = recognizer.listen(source)
-
+            # recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            audio = recognizer.listen(source, phrase_time_limit=2)
 
             text = recognizer.recognize_google(audio)
             text = text.lower()
             
 
-            speaker.say("You said: " + text)
+            print("You said: " + text)
 
             assistant.request(text)
     except sr.UnknownValueError:
-        print("error")
-        # speaker.say("Sorry, I didn't get that.")
         recognizer = sr.Recognizer()
-        # speaker.runAndWait()
+        print("error")
+        speaker.say("Sorry, I didn't get that.")
+        speaker.runAndWait()
